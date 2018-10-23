e775dfdd1a2e243c44d0bb584914dadbf50b253e
Thaddee Tyl <thaddee.tyl@gmail.com>
2018-10-23 21:41:51 +0200

    Add tests for GTP play command

--- test/gtp.js
+++ test/gtp.js
@@ -9,7 +9,13 @@ var Board = Baduk.Board;
   var gtp = new GTP({board});
 
   gtp.exec('play white a19');
+  gtp.exec('play B B19');
+  gtp.exec('play w a18');
+  gtp.exec('play bLACK B18');
 
   assert.equal(gtp.stream.errors.length, 0);
   assert.equal(board.get(0, 0).color, Board.WHITE);
+  assert.equal(board.get(1, 0).color, Board.BLACK);
+  assert.equal(board.get(0, 1).color, Board.WHITE);
+  assert.equal(board.get(1, 1).color, Board.BLACK);
 }
