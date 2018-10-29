7b2eb3f6093dac86089ab7aa4e738133045cc5ea
Thaddee Tyl <thaddee.tyl@gmail.com>
2018-10-29 22:03:22 +0100

    Add help to print-sgf program

--- bin/print-sgf.js
+++ bin/print-sgf.js
@@ -4,6 +4,13 @@ var Baduk = require('../src/sgf.js');
 var everyStep = false;
 process.argv.forEach(function(arg) {
   if (arg === "--steps") { everyStep = true; }
+  if (arg === "--help" || arg === "-h") {
+    console.log("Display a saved Go game in the terminal.");
+    console.log("Usage: print-sgf.js <game.sgf");
+    console.log("  --help, -h: print this help message.");
+    console.log("  --steps: display the screen at each turn.");
+    process.exit(0);
+  }
 });
 
 process.stdin.setEncoding('utf8');
